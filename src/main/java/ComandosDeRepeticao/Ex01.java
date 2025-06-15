355155624792c97f33b1f3d1ae7877f952177b12
GiovanaKayo <giovanakayo14@gmail.com>
2025-06-14 23:57:11 -0300

    Update Comandos de Repetição

---
+++ src/main/java/ComandosDeRepeticao/Ex01.java
@@ -0,0 +1,12 @@
+package ComandosDeRepeticao;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        int n = 1;
+        while (n <= 20) {
+            System.out.println("Eu gosto de estudar Algoritmos!");
+            n++;
+        }
+    }
+}
+
